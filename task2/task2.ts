24b6f7f770650a2fd8ebab34263a41403263a3f8
Brendy <martin.brandejs@gypri.cz>
2025-08-12 11:44:36 +0200

    Initial setup of task2 of internal certification

---
+++ task2/task2.ts
@@ -0,0 +1,87 @@
+import * as readline from 'readline'
+import fs from 'fs'
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
+
+var orders: string[] = JSON.parse(fs.readFileSync('orders.json', 'utf8'))
+
+const menu: string = `--------------------------------
+What would you like to do today? 
+1 - Order
+2 - List orders
+3 - Delete order
+4 - Exit
+
+Today, I would like to: `
+
+function userInput(): void {
+	rl.question(menu, (command) => {
+		switch (command) {
+			case '1':
+			case 'order':
+				placeOrder()
+				break
+			case '2':
+			case 'list':
+				listOrders()
+				break
+			case '3':
+			case 'delete':
+				deleteOrder()
+				break
+			case '4':
+			case 'exit':
+				rl.close()
+				return
+		}
+		setImmediate(userInput)
+	})
+}
+
+function placeOrder(): void {
+	rl.question('What do you wish to order? ', (order) => {
+		orders.push(order)
+		saveToJSON(orders, 'orders.json')
+		console.log(`The order for ${order} has been placed.`)
+
+		setImmediate(userInput)
+	})
+}
+
+function listOrders(): void {
+	console.log('Here is the list of orders: ')
+	for (const order of orders) {
+		console.log(`- ${order}`)
+	}
+
+	setImmediate(userInput)
+}
+
+function deleteOrder(): void {
+	rl.question('What do you wish to remove? ', (order) => {
+		var deletedOrder: string | undefined = undefined
+
+		if (orders.includes(order)) {
+			deletedOrder = order
+			orders.splice(orders.indexOf(order), 1)
+		} else if (0 < Number(order) && Number(order) <= orders.length) {
+			deletedOrder = orders.splice(Number(order) - 1, 1)[0]
+		} else {
+			console.log('Order not found.')
+			return setImmediate(userInput)
+		}
+
+		console.log(`${deletedOrder} has been removed.`)
+		saveToJSON(orders, 'orders.json')
+		setImmediate(userInput)
+	})
+}
+
+function saveToJSON(arrayToSave: string[], fileName: string): void {
+	fs.writeFileSync(fileName, JSON.stringify(arrayToSave))
+}
+
+userInput()
